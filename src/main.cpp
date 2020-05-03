baa0113958a13dd8af069fec2f8497d12288ef8a
Gaurav Singh <singhgaurav1302@gmail.com>
2020-05-03 23:36:55 +0200

    Add CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,44 @@
 #include "Character.h"
 #include "Game.h"
 
-void test()
+void auto_test()
 {
-    Game game;
-    game.init();
-    game.startEnemyAttack();
+  Game game;
+  game.init();
+  game.startEnemyAttack();
 
-    auto players = game.getPlayers();
-    auto player = std::begin(players);
+  auto players = game.getPlayers();
+  auto player = std::begin(players);
 
-    int iterations = 10;
-    while ((player != std::end(players)) && player->isAlive() && iterations-- > 0)
-    {
-        //it takes 8 * 2600 milliseconds to kill player
-        //4 turns to kill Orc and 
-        //10 turns to kill Dragon
+  int iterations = 10;
+  while ((player != std::end(players)) && player->isAlive() && iterations-- > 0)
+  {
+    //it takes 8 * 2600 milliseconds to kill player
+    //4 turns to kill Orc and 
+    //10 turns to kill Dragon
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(2600));
-        player->attackOpponent(game.getEnemyByName("Orc"));
-        player->attackOpponent(game.getEnemyByName("Dragon"));
-    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    player->attackOpponent(game.getEnemyByName("Orc"));
+    player->attackOpponent(game.getEnemyByName("Dragon"));
+  }
 
-    game.printScoreCard();
-    if (player->isAlive())
-    {
-        std::cout << "Test Passed" << std::endl;
-    }
-    else
-    {
-        std::cout << "Test Failed" << std::endl;    
-    }   
+  game.printScoreCard();
+  if (player->isAlive())
+  {
+    std::cout << "Test Passed" << std::endl;
+  }
+  else
+  {
+    std::cout << "Test Failed" << std::endl;    
+  }   
 }
 
 int main()
 {
-    //test();
-    Game game;
-    game.init();
-    game.play();
+  auto_test();
+
+  //manually kill monsters by enabling below
+  //Game game;
+  //game.init();
+  //game.play();
 }
